6a9a7b7d49129f4650725e959ed633bf57696d57
Lukas von Blarer <lukas.vonblarer@gmail.com>
2016-10-12 14:33:26 +0200

    Fixing JS error

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@
 \******************************************/
 
 import * as anchorScrollAnimation  from './misc/anchorScrollAnimation';
-import * as buggyFill              from './misc/buggyFill';
+import * as buggyfill              from './misc/buggyfill';
 import * as fastclick              from './misc/fastclick';
 import * as headroom               from './misc/headroom';
 import * as magnificMessages       from './misc/magnificMessages';
 import * as responsiveMenu         from './misc/responsiveMenu';
 
 anchorScrollAnimation.init();
-buggyFill.init();
+buggyfill.init();
 fastclick.init();
 headroom.init();
 magnificMessages.init();
