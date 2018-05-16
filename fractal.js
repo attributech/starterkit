e4a01c7378ff90c16d2215d2a22918213e919dd3
Lukas von Blarer <lukas.vonblarer@gmail.com>
2018-05-16 11:27:02 +0200

    First working version of fractal

---
+++ fractal.js
@@ -0,0 +1,45 @@
+const pkg = require('./package.json');
+const fractal = require('@frctl/fractal').create();
+const twigAdapter = require('@wondrousllc/fractal-twig-drupal-adapter');
+const twig = twigAdapter({
+  handlePrefix: '@components/',
+});
+
+const paths = {
+  build: `${__dirname}/styleguide`,
+  docs: `${__dirname}/docs`,
+  components: `${__dirname}/components`,
+  static: `${__dirname}/dist`,
+};
+
+fractal.set('project.title', pkg.name);
+fractal.set('project.version', pkg.version);
+fractal.set('project.author', pkg.author);
+
+fractal.components.engine(twig);
+fractal.components.set('default.preview', '@preview');
+fractal.components.set('ext', '.twig');
+fractal.components.set('path', paths.components);
+
+fractal.components.set('resources', {
+  scss: {
+    label: 'SCSS',
+    match: ['**/*.scss']
+  },
+  css: {
+    label: 'CSS',
+    match: ['**/*.css']
+  },
+  other: {
+    label: 'Other Assets',
+    match: ['**/*', '!**/*.scss', '!**.css']
+  }
+});
+
+fractal.docs.set('path', paths.docs);
+
+fractal.web.set('static.path', paths.static);
+fractal.web.set('static.mount', 'dist');
+fractal.web.set('builder.dest', paths.build);
+
+module.exports = fractal;
--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,9 @@ var notify = require('gulp-notify');
 
 var sassGlob = require('gulp-sass-glob');
 
+const fractal = require('./fractal.js');
+const logger = fractal.cli.console; // keep a reference to the fractal CLI console utility
+
 // Set paths
 var dist = 'dist/';
 var path = {
@@ -194,8 +197,48 @@ gulp.task('modernizr', function () {
     .pipe(gulp.dest(path.modernizr.dist))
 });
 
+/*
+ * Start the Fractal server
+ *
+ * In this example we are passing the option 'sync: true' which means that it will
+ * use BrowserSync to watch for changes to the filesystem and refresh the browser automatically.
+ * Obviously this is completely optional!
+ *
+ * This task will also log any errors to the console.
+ */
+
+gulp.task('fractal:start', function () {
+  const server = fractal.web.server({
+    sync: true
+  });
+  server.on('error', err => logger.error(err.message));
+  return server.start().then(() => {
+    logger.success(`Fractal server is now running at ${server.url}`);
+  });
+});
+
+/*
+ * Run a static export of the project web UI.
+ *
+ * This task will report on progress using the 'progress' event emitted by the
+ * builder instance, and log any errors to the terminal.
+ *
+ * The build destination will be the directory specified in the 'builder.dest'
+ * configuration option set above.
+ */
+
+gulp.task('fractal:build', function () {
+  const builder = fractal.web.builder();
+  builder.on('progress', (completed, total) => logger.update(`Exported ${completed} of ${total} items`, 'info'));
+  builder.on('error', err => logger.error(err.message));
+  return builder.build().then(() => {
+    logger.success('Fractal build completed!');
+  });
+});
+
 var defaultTasks = [
   'svg',
+  'fractal:build',
 ];
 
 var developTasks = [
@@ -203,6 +246,7 @@ var developTasks = [
   'webpack:develop',
   'watch',
   'browsersync',
+  'fractal:start',
 ];
 
 var productionTasks = [
