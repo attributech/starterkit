7701f8233015bfc8c2e664d1d212fad4b054a47c
Lukas von Blarer <lukas.vonblarer@gmail.com>
2019-03-07 12:30:53 +0100

    Adding improvements from a recent project

---
+++ components/03-modules/file/file.config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const faker = require('faker');
+
+module.exports = {
+  context: {
+    description: 'Press images',
+    file_type: 'ZIP',
+    file_size: '108.76 KB',
+    url: '#',
+  }
+};
---
+++ components/03-modules/link-button/link-button.config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const faker = require('faker');
+
+module.exports = {
+  context: {
+    url: '#',
+    text: 'Download',
+  }
+};
